4cdaacd69a984ca418e36a0e8ed3857d32463fa2
Evgeniy <zenbysel@gmail.com>
2022-05-15 13:37:52 +0300

    fix: update header for cloudfront invalidation

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           <Link className={classes.homeLink} to="/">
-            My Store!
+            Updated store!
           </Link>
         </Typography>
 
