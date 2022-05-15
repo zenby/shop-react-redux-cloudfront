b75aebde14510eb36b2b1f092966fd02a62a8840
Evgeniy Dainovich <evg.dainovich@gmail.com>
2022-05-15 13:26:21 +0300

    fix: remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import 'index.css'
 import App from 'components/App/App'
 import {store} from 'store/store'
 import {Provider} from 'react-redux'
-import * as serviceWorker from './serviceWorker'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import axios from 'axios'
 
@@ -30,8 +29,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
