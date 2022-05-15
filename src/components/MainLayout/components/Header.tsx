60201b1705d1583ab56054c6196aeb573248d64f
Evgeniy <zenbysel@gmail.com>
2022-05-15 15:49:46 +0300

    fix: add serverless deployment

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           <Link className={classes.homeLink} to="/">
-            Updated store!
+            Header for task 2
           </Link>
         </Typography>
 
