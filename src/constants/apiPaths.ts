7800f942642f73fc5fa3a85b6b1b2d9650bfe13c
Evgeniy Dainovich <zenbysel@gmail.com>
2022-06-12 13:55:50 +0300

    feat: align products with api gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://e0655w3jr6.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
