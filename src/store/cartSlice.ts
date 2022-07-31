68661e5a97406f715793e709c8166dd2a72f5d55
zenby <zenbysel@gmail.com>
2022-07-31 16:29:11 +0300

    fix: wire cart service with frontend app

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -57,7 +57,7 @@ export const addToCart = (product: Product) => async (dispatch: any, getState: a
     cart: {items},
   } = getState()
   await axios.put(
-    `${API_PATHS.cart}/profile/cart`,
+    API_PATHS.cartProfile,
     {items},
     {
       headers: {
@@ -73,7 +73,7 @@ export const removeFromCart = (product: Product) => async (dispatch: any, getSta
     cart: {items},
   } = getState()
   await axios.put(
-    `${API_PATHS.cart}/profile/cart`,
+    API_PATHS.cartProfile,
     {items},
     {
       headers: {
@@ -89,7 +89,7 @@ export const clearCart = () => async (dispatch: any, getState: any) => {
     cart: {items},
   } = getState()
   await axios.put(
-    `${API_PATHS.cart}/profile/cart`,
+    API_PATHS.cartProfile,
     {items},
     {
       headers: {
